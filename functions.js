a44ffe17047c5ab70105a9d65c024a1cf31b1daa
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-01-26 11:50:11 -0500

    upload

---
+++ functions.js
@@ -0,0 +1,10 @@
+/*
+    =================================
+    Author: Patrick
+    File: functions.html
+    Title: Functions
+    Modified by: N/A
+    Date: 10 30 2022
+    Description: snippets for different purposes
+    =================================
+*/
